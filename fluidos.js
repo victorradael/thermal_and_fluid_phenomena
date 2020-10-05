80b27755a2a68e316ceb5cf7490486832e37f192
Victor Radael <victorradael@hotmail.com>
2020-10-05 15:26:33 -0300

    :rocket: Q01

---
+++ fluidos.js
@@ -0,0 +1,28 @@
+// 1- Segundo a Agência Nacional de Petróleo (ANP), o etanol fornecido pelas bombas de abastecimentos em postos
+// de combustı́veis deve ter uma densidade ρ et entre 0,805 g/cm 3 e 0,811 g/cm 3 . Para verificar se existe adulteração
+// no combustı́vel, instala-se um densı́metro que consiste de um cilindro com duas esferas de densidades diferentes,
+// ligeiramente maior e menor do que ρ et . Na Figura abaixo podemos ver três amostras de combustı́vel. Uma amostra
+// é de etanol puro e as outras duas amostras são misturas.
+
+// AMOSTRA I - As duas esferas estão no fundo do recipiente
+// AMOSTRA II - As duas esferas estão flutuando no recipiente
+// AMOSTRA III - Uma das esferas esta flutuando e a outra esta no fundo do recipiente
+
+//(a) Relacione as densidades do etanol e das duas esferas.
+
+// Supõe-se que as esferas tem densidade:
+// ESFERA 1: 0,800 g/cm 3; --> levemente menor que a minima prevista pelo enunciado
+// ESFERA 2: 0,820 g/cm 3; --> levemente maior que a maxima prevista pelo enunciado
+
+//(b) Qual amostra está dentro dos padrões estabelecido pela ANP? Explique seu raciocı́nio.
+
+// A amostra que esta dentro dos padroes estabelecidos pela ANP, eh a amostra III,
+// pois a esfera com a densidade levemente superior ao maximo previsto esta localizada
+// ao fundo do recipiente e a esfera com a densidade levemente inferior ao minimo previsto
+// encontra-se flutuando no recipiente.
+
+//(c) Relacione as densidades do etanol e das duas misturas.
+
+// A Amostra I possui densidade inferior ao mino previsto, e o que indica isso eh
+// o fato das duas esferas terem afundado no recipiente. Ja na Amostra II a densidade
+// excede o maximo previsto, o que faz com que as duas esferas flutuem no recipiente.
